062d0df52ca1f4d2cfd580aa87216e1530caf356
salinpaul valooran <salinpv@hotmail.com>
2023-11-06 01:32:52 +0530

    array for Months days and setting day value to show January, February, March etc

--- version_01/index.js
+++ version_01/index.js
@@ -6,7 +6,9 @@ let year = document.getElementById("year");
 let today = new Date();
 
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 date.innerHTML = today.getDate();
 day.innerHTML = weekDays[today.getDay()];
-month.innerHTML = today.getMonth();
+month.innerHTML = months[today.getMonth()];
 year.innerHTML = today.getFullYear();
