846f4dcf9844ee6b1522d4ccd229b417918363c1
salinpaul valooran <salinpv@hotmail.com>
2023-11-06 01:21:24 +0530

    variable for getting todays date

--- version_01/index.js
+++ version_01/index.js
@@ -2,3 +2,5 @@ let date = document.getElementById("date");
 let day = document.getElementById("day");
 let month = document.getElementById("month");
 let year = document.getElementById("year");
+
+let today = new Date();
