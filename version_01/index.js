65265fe534490a63a39130c59f8c8075819a3f5c
salinpaul valooran <salinpv@hotmail.com>
2023-11-06 01:38:49 +0530

    terniary operator to show date in 00 if today.getDay() is less than 10

--- version_01/index.js
+++ version_01/index.js
@@ -8,7 +8,7 @@ let today = new Date();
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-date.innerHTML = today.getDate();
+date.innerHTML = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate();
 day.innerHTML = weekDays[today.getDay()];
 month.innerHTML = months[today.getMonth()];
 year.innerHTML = today.getFullYear();
